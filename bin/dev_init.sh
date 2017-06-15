83d357c02bd3c7f08b69cb39b01f7ecc711dc370
jodli <job87@web.de>
2017-06-15 22:40:14 +0200

    Moved init cleanup to php container.

--- bin/dev_init.sh
+++ bin/dev_init.sh
@@ -3,11 +3,17 @@
 cd "$(dirname ${BASH_SOURCE[0]})"
 cd ..
 
-rm -rf var/cache/*
-rm -rf web/bundles
-rm -rf web/css
-rm -rf web/js
+echo "Cleaning dependencies."
+docker-compose exec php bash -c "rm -rf var/cache/* web/bundles web/css web/js"
+
+echo "composer install"
 docker-compose exec php bash -c "cd /var/www && composer install"
+
+echo "npm install"
 docker-compose exec php bash -c "cd /var/www/app/Resources/node_tools && npm install"
+
+echo "assentic:dump"
 docker-compose exec php php ../bin/console assetic:dump
+
+echo "doctrine:schema:create"
 docker-compose exec php php ../bin/console doctrine:schema:create
