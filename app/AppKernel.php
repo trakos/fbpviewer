523a80f121890f746810a52ebb5037a09dc10e59
Piotr Stankowski <git@trakos.pl>
2022-03-04 17:06:56 +0100

    Update to php 7.4, remove sensio distribution and generator bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,12 +22,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                //$bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
         }
 
         return $bundles;
@@ -40,16 +34,16 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
--- bin/dev_init.sh
+++ bin/dev_init.sh
@@ -9,5 +9,8 @@ docker-compose exec php rm -rf var/cache/* web/bundles web/css web/js web/build
 echo "composer install"
 docker-compose exec php bash -c "cd /var/www && composer install"
 
+echo "composer install"
+docker-compose exec php bash -c "cd /var/www && bin/console cache:clear"
+
 echo "doctrine:schema:create"
 docker-compose exec php php ../bin/console doctrine:schema:create
